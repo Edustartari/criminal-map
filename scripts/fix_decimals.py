2cfd27a939b2613ea3bf0f98a64d36a62565f87a
Eduardo <edustartari@gmail.com>
2023-12-01 23:32:51 -0300

    fix decimals from sheets and json files

---
+++ scripts/fix_decimals.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import os
+
+# We'll check each cell in each sheet
+# IF decimal number has 3 digits after the comma, we'll add a zero after the comma
+# Example: 6.159 -> 6159.0
+
+# Get path to assets folder
+assets_path = os.path.join(os.path.dirname(__file__), '../assets/sheets')
+
+# Get all folders inside assets folder
+folders = os.listdir(assets_path)
+
+# For each folder
+for folder in folders:
+    print('')
+    print(folder)
+    
+    if folder != '565':
+        continue
+
+    print('Fixing decimals...')
+    # Get path to folder
+    folder_path = os.path.join(assets_path, folder)
+    # Get all files inside folder
+    files = os.listdir(folder_path)
+
+    if len(files) == 23:
+        for file in files:
+            file_path = os.path.join(folder_path, file)
+            df = pd.read_csv(file_path)
+            # Get each cell in each sheet
+            for index, row in df.iterrows():
+                for column in df.columns:
+                    # Check if cell is a float
+                    if isinstance(row[column], float):
+                        # Check if cell has 3 digits after the comma
+                        if len(str(row[column]).split('.')[1]) == 3:
+                            # Add a zero after the comma
+                            df.loc[index, column] = float(str(row[column]).replace('.', ''))
+                        # Check if cell has 2 digits after the comma
+                        elif len(str(row[column]).split('.')[1]) == 2:
+                            # Add two zeros after the comma
+                            df.loc[index, column] = float(str(row[column]).replace('.', '') + '0')
+                        # Check if cell has 1 digit after the comma and the number is not 0
+                        elif len(str(row[column]).split('.')[1]) == 1 and str(row[column]).split('.')[1] != '0':
+                            # Add three zeros after the comma
+                            df.loc[index, column] = float(str(row[column]).replace('.', '') + '00')
+
+            # Save file at the same path
+            df.to_csv(file_path, index=False)
+            
+            # Convert to json and save inside assets/json folder
+            df.to_json(f'assets/json/{folder}/{file[:-4]}.json', orient='records')
+
+
+
+
