ecc8402ad97911e92d3e33dfc0112a04a501a582
Edustartari <edustartari@gmail.com>
2025-12-02 21:52:45 -0300

    add new config into githubactions to build with docker cache and push to docker hub

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState/* , useEffect */ } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '@/app/page.module.css';
 import '@/app/styles/home.css';
